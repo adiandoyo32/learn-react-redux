5523e5d18fcade4520ce6f1005fe43a45acb0b2c
adiandoyo.dl32 <adiandoyo.dl32@gmail.com>
2022-01-04 13:10:59 +0700

    first commit

---
+++ src/pages/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <p>Name: </p>
+            <p>Age: </p>
+            <p>Email: </p>
+            <p>Name: </p>
+        </div>
+    )
+}
+
+export default Profile
